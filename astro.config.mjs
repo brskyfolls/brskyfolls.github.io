f58cbc844d0de3dc998570ec8d5aa130d75e99c2
Rodrigo Arismendi <rodrigo.arismendi.uptp@gmail.com>
2024-08-13 12:17:41 -0300

    changes for astro deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://brskyfolls.github.io',
+	base: 'brskyfolls.github.io',
 	integrations: [mdx(), sitemap()],
 	devToolbar: {
 		enabled: false
